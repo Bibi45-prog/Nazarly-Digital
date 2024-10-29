381235948b35882a3813f220c3758d1851c5529f
bibigozel <Bibi45-prog>
2024-10-29 18:31:54 +0500

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -215,13 +215,13 @@ export default function Home() {
       </div>
 
       <div
-        className="relative flex flex-col justify-center items-center h-[80vh] sm:h-[60vh] md:h-[50vh] bg-contain bg-center bg-no-repeat"
+        className="relative flex flex-col justify-center items-center h-[80vh] sm:h-[60vh] md:h-[50vh] bg-contain bg-center bg-no-repeat my-20"
         style={{ backgroundImage: "url('/assets/img/blur_bg.svg')" }}
       >
         <div className="absolute inset-0 bg-black/40 backdrop-blur-2xl"></div>
 
         <div className="relative z-10 text-white text-center">
-          <h1 className="text-lg sm:text-md">
+          <h1 className="text-5xl sm:text-md">
             We will design the first screen <br /> of the site for free
           </h1>
           <img
